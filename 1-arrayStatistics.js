65bcc999141fb5adaa09f2b8a69a1273eccc566c
Dushime Paulette <p.dushime12@gmail.com>
2024-12-09 10:23:56 +0200

    updates

--- 1-arrayStatistics.js
+++ 1-arrayStatistics.js
@@ -1,3 +1,15 @@
+/*You are given an array of numbers. Your task is to implement a function called arrayStats that returns an object containing various statistics about the array.
+
+The returned object should have the following properties:
+sum: The sum of all numbers in the array.
+average: The average of all numbers in the array (rounded to two decimal places).
+min: The minimum value in the array.
+max: The maximum value in the array.
+
+Example:
+arrayStats([1, 2, 3, 4, 5]); // should return { sum: 15, average: 3.00, min: 1, max: 5 }
+*/
+
 function arrayStats(arrayNumbers) {
   let sum = 0;
   let min = arrayNumbers[0];
@@ -18,5 +30,5 @@ function arrayStats(arrayNumbers) {
     min: min,
     max: max,
   };
-  console.log(arrayStats([1, 2, 3, 4, 5]));
 }
+console.log(arrayStats([1, 2, 3, 4, 5]));
