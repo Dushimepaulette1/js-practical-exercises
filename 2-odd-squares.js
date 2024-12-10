1903719fd55d6ec41adc722bae6ac3197f104635
Dushime Paulette <p.dushime12@gmail.com>
2024-12-10 10:43:43 +0200

    updates on oddsquare

--- 2-odd-squares.js
+++ 2-odd-squares.js
@@ -4,14 +4,11 @@ Example:
 oddSquares([1, 2, 3, 4, 5, 6]); // should return [1, 9, 25]
 oddSquares([10, 15, 20, 25, 30]); // should return [225, 625]
 */
-function oddSquares(arr) {
-  let evenNumbers = arr
-    .filter(function (arrayNum) {
-      return arrayNum % 2 !== 0;
-    })
-    .map(function (num) {
-      return num * num;
-    });
-  return evenNumbers;
+function oddSquares(array) {
+  for (let i = 0; i < array.length; i++) {
+    let evenNumbers = array.filter((num) => num % 2 !== 0);
+    let squares = evenNumbers.map((number) => number ** 2);
+    return squares;
+  }
 }
-console.log(oddSquares([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
+console.log(oddSquares([3, 8, 9, 1, 3]));
